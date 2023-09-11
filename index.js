19bd4e4b1c7de77b614fe844fac2a71522dce9bb
Jesus Tapia <chucholuco@gmail.com>
2023-09-11 10:25:46 -0600

    adding layout and home page

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
+const path = require('path')
+const router = require('./routes')
 
 const app = express()
 
-app.use('/', (req, res) => {
-    res.send('Funciona')
-}) 
+// habilitar handlebars como view
+app.engine('handlebars',
+    exphbs.engine({
+        defaultLayout: 'layout'
+    })
+)
+
+app.set('view engine', 'handlebars')
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', router())
+
 
 app.listen(5000)
