951e0e02d5be6f728c1b8ef11f24e6017b11013e
Jesus Tapia <chucholuco@gmail.com>
2023-09-14 14:23:36 -0600

    errors handler

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const MongoStore = require('connect-mongo')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
 const flash = require('connect-flash')
+const createError = require('http-errors')
 const passport = require('./config/passport')
+
 
 require('dotenv').config({path: 'variables.env'})
 
@@ -59,5 +61,18 @@ app.use((req, res, next) => {
 
 app.use('/', router())
 
+// 404 pagina no existente
+app.use((req, res, next) => {
+    next(createError(404, 'No Encontrado'));
+})
+
+// Administración de los errores
+app.use((error, req, res, next) => {
+    res.locals.mensaje = error.message;
+    const status = error.status || 500;
+    res.locals.status = status;
+    res.status(status);
+    res.render('error');
+});
 
 app.listen(process.env.PUERTO)
